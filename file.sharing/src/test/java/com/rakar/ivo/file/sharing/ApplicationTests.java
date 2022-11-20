590a89d8e0dbcfc277ab4f16950e05c266e6c8ab
ivo206 <ivo23061987@gmail.com>
2022-11-20 19:22:43 +0000

    First commit

---
+++ file.sharing/src/test/java/com/rakar/ivo/file/sharing/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakar.ivo.file.sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
